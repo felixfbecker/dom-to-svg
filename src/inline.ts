bab885167a0161c313635b27ad68a46aaaf8c4ea
Felix Becker <felix.b@outlook.com>
2020-10-31 17:23:20 +0100

    Add accept option to fetchAsDataURL()

--- src/inline.ts
+++ src/inline.ts
@@ -1,7 +1,25 @@
-export async function fetchAsDataURL(url: string): Promise<URL> {
-	const response = await fetch(url)
+import { is as isContentType } from 'type-is'
+
+export interface FetchAsDataURLOptions {
+	accept?: string[]
+}
+
+export async function fetchAsDataURL(url: string, options: FetchAsDataURLOptions = {}): Promise<URL> {
+	if (!url) {
+		throw new Error('No URL passed')
+	}
+	const headers = new Headers()
+	if (options.accept) {
+		headers.set('Accept', options.accept.join(', '))
+	}
+	const response = await fetch(url, { headers })
 	if (!response.ok) {
 		throw new Error(response.statusText)
+	}
+	const contentType = response.headers.get('Content-Type')
+	// Do an additional client-side check, in case the server does not handle the Accept header.
+	if (options.accept && (!contentType || !isContentType(contentType, options.accept))) {
+		throw new Error(`Content-Type not accepted: ${String(contentType)}`)
 	}
 	const blob = await response.blob()
 	const reader = new FileReader()
