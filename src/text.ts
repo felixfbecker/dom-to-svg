91bed46a2a8117763627d8f2760afe09e898e72f
Felix Becker <felix.b@outlook.com>
2020-11-24 10:11:37 +0100

    fix(text): clear selection afterwards

--- src/text.ts
+++ src/text.ts
@@ -56,6 +56,7 @@ export function handleTextNode(textNode: Text, context: TraversalContext): void 
 				textSpan.textContent = selection.toString()
 			} finally {
 				parentElement.style.userSelect = previousUserSelect
+				selection.removeAllRanges()
 			}
 
 			textSpan.setAttribute('x', lineRectangle.x.toString())
