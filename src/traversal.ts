1aba20222866bca24008683edea224ef4335b34a
Felix Becker <felix.b@outlook.com>
2021-02-13 16:12:51 +0100

    fix: handle position: absolute elements inside overflow: hidden

--- src/traversal.ts
+++ src/traversal.ts
@@ -23,6 +23,12 @@ export interface TraversalContext {
 	readonly currentSvgParent: SVGElement
 	readonly parentStackingLayer: SVGGElement
 	readonly stackingLayers: StackingLayers
+
+	/**
+	 * Masks for ancestor elements (that are `overflow: hidden`) affecting the current element, from closest to furthest.
+	 */
+	readonly ancestorMasks: { mask: SVGMaskElement; forElement: Element }[]
+
 	readonly labels: Map<HTMLLabelElement, string>
 	readonly getUniqueId: (prefix: string) => string
 	readonly options: Required<DomToSvgOptions>
